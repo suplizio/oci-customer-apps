f4ea2156a124f6858aa126d67db8736e99a00f9d
Jason Suplizio <jason.suplizio@oracle.com>
2018-10-14 16:12:47 -0700

    displaying multiple ips

--- html/static/serverdata.js
+++ html/static/serverdata.js
@@ -4,10 +4,10 @@ request.onreadystatechange = function() {
   if(this.readyState == this.HEADERS_RECEIVED) {
 
     var backendIp = request.getResponseHeader("backend-ip");
-    var backendIp = request.getResponseHeader("host-ip");
+    var lb-ip = request.getResponseHeader("host-ip");
 
     document.getElementById("backend-ip").innerHTML = backendIp;
-    document.getElementById("lb-ip").innerHTML = backendIp;
+    document.getElementById("lb-ip").innerHTML = lb-ip;
     var headers = request.getAllResponseHeaders();
     var arr = headers.trim().split(/[\r\n]+/);
     var headerMap = {};
